eda4283228275268758fee0225c25e2306bdd8ec
baej-ultra <pigmejov@gmail.com>
2025-02-05 09:40:01 +0100

    Init commit

---
+++ src/test/java/org/bromanowski/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bromanowski.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
